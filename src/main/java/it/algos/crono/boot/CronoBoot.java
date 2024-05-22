da10a3dadf73800288cd561a9fbb9bad5bfa102a
gac <info@algos.it>
2024-05-22 18:36:12 +0200

    - varie

--- src/main/java/it/algos/crono/boot/CronoBoot.java
+++ src/main/java/it/algos/crono/boot/CronoBoot.java
@@ -1,16 +1,7 @@
 package it.algos.crono.boot;
 
-import it.algos.crono.mese.*;
 import it.algos.vbase.backend.boot.*;
-import static it.algos.vbase.backend.boot.BaseCost.*;
-import static it.algos.vbase.backend.boot.BaseVar.*;
-import it.algos.vbase.backend.logic.*;
-import it.algos.vbase.backend.pref.*;
-import it.algos.vbase.backend.wrapper.*;
-import it.algos.vbase.ui.view.*;
 import org.springframework.stereotype.*;
-
-import java.util.*;
 
 /**
  * Project wiki24
@@ -26,58 +17,6 @@ public class CronoBoot extends BaseBoot {
 
     public CronoBoot() {
     }
-
-
-//    /**
-//     * Aggiunge al menu le @Route (view) standard e specifiche <br>
-//     * Questa classe viene invocata PRIMA della chiamata del browser <br>
-//     * <p>
-//     * Nella sottoclasse che invoca questo metodo, aggiunge le @Route (view) specifiche dell' applicazione <br>
-//     * Le @Route vengono aggiunte ad una Lista statica mantenuta in BaseVar <br>
-//     * Verranno lette da MainLayout la prima volta che il browser 'chiama' una view <br>
-//     * Può essere sovrascritto, invocando PRIMA il metodo della superclasse <br>
-//     */
-//    @Override
-//    protected void fixRoutes() {
-//        super.fixRoutes();
-//
-//        List<Class> listaViewsProject;
-//        String message;
-//        String viewName;
-//        String pathView="it.algos.crono";
-//
-//        if (Pref.usaMenuAutomatici.is()) {
-//            listaViewsProject = reflectionService.getSubClazz(CrudView.class,pathView);
-//            if (listaViewsProject != null) {
-//                for (Class clazz : listaViewsProject) {
-//                    if (annotationService.usaMenuAutomatico(clazz)) {
-////                        viewClazzListProject.add(clazz);
-//                        viewName = clazz.getSimpleName();
-//                        viewName = textService.levaCoda(viewName, SUFFIX_VIEW);
-////                        nameViewListProject.add(viewName);
-//                    }
-//                }
-//            }
-//            else {
-//                message = String.format("Non esiste nessuna view/route nel progetto [%s]", projectCurrent);
-//                logger.warn(new WrapLog().exception(new Exception(message)));
-//            }
-//        }
-//        else {
-////            viewClazzListProject.add(MeseView.class);
-//        }
-//    }
-
-
-//    protected boolean checkResetStartup() {
-//        if (super.checkResetStartup()) {
-//            for (ModuloService modulo : moduloServiceListProject) {
-//                modulo.checkResetStartup();
-//            }
-//        }
-//
-//        return false;
-//    }
 
 
 }
