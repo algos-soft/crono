21b7daa706a09209be410bbd4a5cdf6f16a950a8
gac <info@algos.it>
2024-05-24 07:15:00 +0200

    -  EqualsAndHashCode

--- src/main/java/it/algos/crono/mese/MeseEntity.java
+++ src/main/java/it/algos/crono/mese/MeseEntity.java
@@ -6,12 +6,10 @@ import it.algos.vbase.backend.entity.*;
 import it.algos.vbase.backend.enumeration.*;
 import lombok.*;
 import org.springframework.data.mongodb.core.index.*;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.*;
-import org.springframework.stereotype.*;
 
-@Component
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
