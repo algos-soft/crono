9207d23ef797db4e7fbdacabd1f0608c1a7b41f6
gac <info@algos.it>
2024-07-10 08:42:29 +0200

    addSearchHeaderx

--- src/main/java/it/algos/crono/mese/MeseList.java
+++ src/main/java/it/algos/crono/mese/MeseList.java
@@ -2,9 +2,6 @@ package it.algos.crono.mese;
 
 import it.algos.crono.list.CronoList;
 import it.algos.vbase.backend.annotation.AList;
-import it.algos.vbase.backend.searchfield.QuickSearch;
-import it.algos.vbase.backend.searchfield.TextSearch;
-import jakarta.annotation.PostConstruct;
 
 import static it.algos.vbase.backend.boot.BaseCost.*;
 
@@ -19,14 +16,6 @@ public class MeseList extends CronoList {
         super(parentView);
     }
 
-    @PostConstruct
-    private void init() {
-
-        getListToolbar().add(new QuickSearch<>(grid));
-
-        grid.addSearchField(new TextSearch(entityClass, "sigla", annotationService.typeTextSearch(entityClass, "sigla")));
-        grid.addSearchField(new TextSearch(entityClass, "nome"));
-    }
 
     @Override
     public void syncHeader() {
