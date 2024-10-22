2498a1e2a4a7514a42858de8e694ac8a3eb0e9a7
gac <info@algos.it>
2024-10-22 18:59:16 +0200

    - ...

--- src/main/java/it/algos/crono/anno/AnnoView.java
+++ src/main/java/it/algos/crono/anno/AnnoView.java
@@ -2,9 +2,9 @@ package it.algos.crono.anno;
 
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.Route;
-import it.algos.vbase.backend.annotation.AView;
+import it.algos.vbase.backend.annotation.IView;
 import it.algos.vbase.backend.constant.Gruppo;
-import it.algos.vbase.ui.view.CrudView;
+import it.algos.vbase.ui.view.AView;
 import it.algos.vbase.ui.view.MainLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,8 +18,8 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Route chiamata dal menu generale o dalla barra del browser <br>
  */
 @Route(value = "anno", layout = MainLayout.class)
-@AView(menuGroup = Gruppo.CRONO, menuName = "Anni", vaadin = VaadinIcon.CALENDAR)
-public class AnnoView extends CrudView {
+@IView(menuGroup = Gruppo.CRONO, menuName = "Anni", vaadin = VaadinIcon.CALENDAR)
+public class AnnoView extends AView {
 
 
     AnnoView(@Autowired AnnoService moduloService) {
--- src/main/java/it/algos/crono/secolo/SecoloView.java
+++ src/main/java/it/algos/crono/secolo/SecoloView.java
@@ -2,9 +2,9 @@ package it.algos.crono.secolo;
 
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.router.Route;
-import it.algos.vbase.backend.annotation.AView;
+import it.algos.vbase.backend.annotation.IView;
 import it.algos.vbase.backend.constant.Gruppo;
-import it.algos.vbase.ui.view.CrudView;
+import it.algos.vbase.ui.view.AView;
 import it.algos.vbase.ui.view.MainLayout;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -19,8 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Route chiamata dal menu generale o dalla barra del browser <br>
  */
 @Route(value = "secolo", layout = MainLayout.class)
-@AView(menuGroup = Gruppo.CRONO, menuName = "Secoli", vaadin = VaadinIcon.CALENDAR)
-public class SecoloView extends CrudView {
+@IView(menuGroup = Gruppo.CRONO, menuName = "Secoli", vaadin = VaadinIcon.CALENDAR)
+public class SecoloView extends AView {
 
 
     SecoloView(@Autowired SecoloService moduloCrudService) {
