b8ed368d9ef8ee88501025815ddaba34501820e1
gac <info@algos.it>
2024-11-21 12:34:48 +0100

    ...

--- src/main/java/it/algos/crono/list/CronoGrid.java
+++ src/main/java/it/algos/crono/list/CronoGrid.java
@@ -3,6 +3,7 @@ package it.algos.crono.list;
 import it.algos.vbase.entity.AbstractEntity;
 import it.algos.vbase.grid.AGrid;
 import it.algos.vbase.grid.ColumnDefinition;
+import it.algos.vbase.logic.ModuloService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -22,8 +23,8 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROT
 @Scope(value = SCOPE_PROTOTYPE)
 public class CronoGrid extends AGrid<AbstractEntity> {
 
-    public CronoGrid(Class<AbstractEntity> entityClazz, List<ColumnDefinition> columnDefinitions) {
-        super(entityClazz, columnDefinitions);
+    public CronoGrid(Class<AbstractEntity> entityClazz, Class formClass, ModuloService moduloService, List<ColumnDefinition> columnDefinitions) {
+        super(entityClazz, formClass, moduloService, columnDefinitions);
     }
 
     @PostConstruct
