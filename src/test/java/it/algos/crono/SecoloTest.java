cef09cfae9c1c20904ee41a1d0459d5916074fd1
gac <info@algos.it>
2025-05-08 21:33:30 +0200

    add main

--- src/test/java/it/algos/crono/SecoloTest.java
+++ src/test/java/it/algos/crono/SecoloTest.java
@@ -1,6 +1,5 @@
 package it.algos.crono;
 
-import it.algos.base.ModuloTest;
 import it.algos.crono.secolo.SecoloEntity;
 import it.algos.crono.secolo.SecoloList;
 import it.algos.crono.secolo.SecoloService;
